c02d7fe2076b7813e81ae40b1246b30ce4bce03a
Antonii Adamovych <antoniiadamovych@gmail.com>
2024-10-25 21:45:10 +0200

    product page

---
+++ src/lib/elements/products/Hero.svelte
@@ -0,0 +1,53 @@
+<script lang="ts">
+</script>
+
+<section>
+    <div class="container">
+        <!-- Hero -->
+        <div class="relative overflow-hidden">
+            <div class="relative z-10">
+                <div
+                    class="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10 lg:py-40"
+                >
+                    <div class="max-w-2xl text-center mx-auto">
+                        <p
+                            style="--anim-delay: 0.2s"
+                            data-anim="fade-in slide-top"
+                            class="inline-block text-sm font-medium bg-clip-text bg-gradient-to-l text-primary"
+                        >
+                            quality price comfort
+                        </p>
+
+                        <!-- Title -->
+                        <div class="mt-5 max-w-2xl">
+                            <h1
+                                data-anim="fade-in"
+                                class="block font-semibold text-4xl md:text-5xl lg:text-6xl text-light"
+                            >
+                                Our Products
+                            </h1>
+                        </div>
+                        <!-- End Title -->
+
+                        <div
+                            class="mt-5 max-w-3xl"
+                            style="--anim-delay: 0.1s"
+                            data-anim="fade-in"
+                        >
+                            <p class="text-lg text-muted">
+                                At DataTech, we specialize in developing,
+                                manufacturing, and delivering high-quality
+                                electrical and electronic cables and connectors.
+                                Our products are trusted across diverse
+                                industries, including mechanical engineering,
+                                factory automation, data processing, networking,
+                                industrial electronics, and telecommunications.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- End Hero -->
+    </div>
+</section>
--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,2 +1,7 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+    import Hero from "$lib/elements/products/Hero.svelte";
+    import Product from "$lib/elements/products/ProductGrid.svelte";
+</script>
+
+<Hero />
+<Product />
